8e9073b8896e5803b51d26875d5c923b1799f212
patelhet1999 <81188132+patelhet1999@users.noreply.github.com>
2021-06-14 21:27:28 +0530

    Create MainActivity.java

---
+++ Day 5/ActivityLifeCycle/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.mydemo_intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("TAG","onCreate called");
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d("TAG","onStart called");
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d("TAG","onResume called");
+    }   @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d("TAG","onPause called");
+    }   @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d("TAG","onStop called");
+    }   @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.d("TAG","onRestart called");
+    }   @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d("TAG","onDestroy called");
+    }
+
+}
